ed3bc355fcb2ea8502ce08b9c4e610dcc50e9f1e
Poowadol Rungtabnapa <51953419+PoowadolRungtabnapa@users.noreply.github.com>
2022-02-27 17:46:46 +0700

    .

---
+++ flutter_navigation_route/lib/main1.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import './folder_screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.arrow_forward),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Text("Page2");
+                }));
+              })
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
